d87fa85f61f823beb7c710b2170dac7226834abb
Mersudin Hasanovic <mhasanovic@eurofunk.com>
2023-11-28 08:34:52 +0100

    Spring Context and its Tests

--- src/test/java/guru/springframework/spring6webapp/Spring6WebappApplicationTests.java
+++ src/test/java/guru/springframework/spring6webapp/Spring6WebappApplicationTests.java
@@ -1,13 +1,37 @@
 package guru.springframework.spring6webapp;
 
+import guru.springframework.spring6webapp.controller.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class Spring6WebappApplicationTests {
 
+	// With @Autowired we say Spring context to inject ApplicationContext for us
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Autowired // inject and ask for an instance of the controller
+	private MyController myController;
+
+	@Test
+	void testAutowireOfController() {
+		assertThat(myController.sayHello()).isEqualTo("Hello Everyone from MyController");
+	}
+
+	@Test
+	void testGetControllerFromContext() {
+		final MyController controller = applicationContext.getBean(MyController.class);
+		assertThat(controller.sayHello()).isEqualTo("Hello Everyone from MyController");
+	}
+
 	@Test
 	void contextLoads() {
+
 	}
 
 }
